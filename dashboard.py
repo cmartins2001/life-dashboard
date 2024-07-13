51834837aee636a50fccca762ce228910b13f9d5
cmartins2001 <cmartinspfhs2001@gmail.com>
2024-07-13 13:09:44 -0400

    added the workout count chart and some basic metrics. whole site needs to be formatted though

--- dashboard.py
+++ dashboard.py
@@ -46,6 +46,11 @@ def create_df(sheets_data):
     return (pd.DataFrame(sheets_data[1:], columns=sheets_data[0]))
 
 
+# Function that returns the mean of a column in a pandas dataframe:
+def get_mean(df, col_name):
+    return (df[col_name].mean())
+
+
 # Main function:
 def main():
     # Connect:
@@ -58,51 +63,71 @@ def main():
     google_df = create_df(raw_data)
 
     # Change the quantitative columns to float or int types:
+
+    # Rating:
     google_df['rating'] = pd.to_numeric(google_df['rating'], errors='coerce')
     google_df['rating'] = google_df['rating'].astype(float)
+
+    # Hours outside:
     google_df['hrs_outside'] = pd.to_numeric(google_df['hrs_outside'], errors='coerce')
     google_df['hrs_outside'] = google_df['hrs_outside'].fillna(0, inplace=False)
 
-    # Print head:
-    # print(google_df.head())
+    # Workout dummy:
+    google_df['workout_bool'] = pd.to_numeric(google_df['workout_bool'], errors='coerce')
+    google_df['workout_bool'] = google_df['workout_bool'].fillna(0, inplace=False)
+
+    # Create a count dataframe for workouts by day of week visual:
+    workout_df = google_df[['day_of_week', 'workout_bool']]
+    workout_count_df = (workout_df
+                        .groupby("day_of_week")
+                        .agg({"workout_bool" : "sum"})
+                        .reset_index()
+                        .rename(columns={"workout_bool" : "workout_count"})
+                        .sort_values(by='workout_count', ascending=False)
+                        )
 
     ### START OF STREAMLIT EDITING ###
 
     # Create a title:
-    st.title("Connor's Life Dashboard")
+    st.title("Connor's :blue[Life] Dashboard")
 
-    # Create streamlit column layout:
-    # col1, col2 = st.columns(2)
+    # Create streamlit column layout for KPIs:
+    col1, col2 = st.columns(2)
 
-    # with col1:
-        # Time series plot of daily rating:
+    with col1:
+        st.metric('Average Daily Rating', f"{get_mean(google_df, 'rating'):.2f}")
+    with col2:
+        st.metric('Hours Outside Per Day', f"{get_mean(google_df, 'hrs_outside'):.2f}")
+    
+    
+    # Time series plot of daily rating:
     st.subheader("Daily Rating")
-    # st.line_chart(data=google_df, x='date', y='rating')
-    rating_line = alt.Chart(google_df).mark_line(color='deepskyblue').encode(
-        x='date',
-        y='rating'
-    )
-    st.altair_chart(rating_line)
+    st.line_chart(data=google_df, x='date', y='rating')
+    # rating_line = alt.Chart(google_df).mark_line(color='deepskyblue').encode(
+    #     x='date',
+    #     y='rating'
+    # )
+    # st.altair_chart(rating_line)
 
     
     # with col2:
     # Bar chart of hours outside over time:
     st.subheader("Hours Spent Outside")
-    # st.bar_chart(data=google_df, x='date', y='hrs_outside')
-    outside_bar = alt.Chart(google_df).mark_bar(color='lightsalmon').encode(
-        x='date',
-        y='hrs_outside'
-    )
-    st.altair_chart(outside_bar)
+    st.bar_chart(data=google_df, x='date', y='hrs_outside')
+    # outside_bar = alt.Chart(google_df).mark_bar(color='lightsalmon').encode(
+    #     x='date',
+    #     y='hrs_outside'
+    # )
+    # st.altair_chart(outside_bar)
 
     # Histogram of workouts by day of week:
     st.subheader("Workouts by Day of Week")
-    # workout_hist = alt.Chart(google_df).mark_bar().encode(
-    #     alt.X("date"),
-    #     y='count()'
+    # workout_hist = alt.Chart(workout_count_df).mark_bar(color='forestgreen').encode(
+    #     x='day_of_week',
+    #     y='workout_count'
     # )
-    ###NOTE: will probably need to create a count data frame using .agg() and then
-    # do the histogram from there ###
+    # st.altair_chart(workout_hist)
+    st.bar_chart(workout_count_df, x='day_of_week', y='workout_count')
     
     st.divider()
 
